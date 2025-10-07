7a3a68a12c294b0873bfeb9a52165b7a5021b733
Akhmetshin Sergei <supergrey16@gmail.com>
2025-10-07 18:54:36 +0300

    Подгрузил файлы gradle

---
+++ app/src/main/java/platform/TKAB/app1234/DelegationService.java
@@ -0,0 +1,14 @@
+package platform.TKAB.app1234;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
